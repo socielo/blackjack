f889c0e6c445661aeb82a3f4bce091dc63f8cb4a
socielo <jnlopez1212@gmail.com>
2023-03-03 18:23:18 -0500

    procedural blackjack game, some bugs

---
+++ procedural_blackjack.py
@@ -0,0 +1,180 @@
+import random as r
+
+# Decided to make this for fun, kind of buggy.
+# Aces are always 11.
+# Dealer stands on 17 always
+# Double down can only draw one extra card
+# One Deck
+# Some blackjack rules may be wrong, haven't played in a year
+
+# Eventually will try to add a gui and implement it into OOP
+
+def blackjack():
+    playable_deck = ['2H-2', '3H-3', '4H-4', '5H-5', '6H-6', '7H-7', '8H-8',
+                     '9H-9', '10H-10', 'AH-11', 'JH-10', 'QH-10', 'KH-10',
+                     '2D-2', '3D-3', '4D-4', '5D-5', '6D-6', '7D-7', '8D-8',
+                     '9D-9', '10D-10', 'AD-11', 'JD-10', 'QD-10', 'KD-10',
+                     '2C-2', '3C-3', '4C-4', '5C-5', '6C-6', '7C-7', '8C-8',
+                     '9C-9', '10C-10', 'AC-11', 'JC-10', 'QC-10', 'KC-10',
+                     '2S-2', '3S-3', '4S-4', '5S-5', '6S-6', '7S-7', '8S-8',
+                     '9S-9', '10S-10', 'AS-11', 'JS-10', 'QS-10', 'KS-10'
+                     ]
+    while True:
+        money = input("How much would you like to deposit? ")
+        try:
+            money = float(money)
+            break
+        except ValueError:
+            print("Please enter a valid number.")
+
+
+
+
+    try:
+        while len(playable_deck) > 0:
+            while True:
+                if money <= 0:
+                    print("You ran out of money!")
+                    quit()
+                print(f"Your balance is {money}")
+                #print(f"Deck: {playable_deck}"
+                print(f"Your balance is {money}")
+                while True:
+                    try:
+                        bet = input("How much would you like to bet? ")
+                        bet = float(bet)
+                        break
+                    except ValueError:
+                        print("Please enter a valid number")
+                if bet > money:
+                    print("You cannot bet more money than you have.")
+                    bet = int(input("What would you like to bet? "))
+
+                dealer_choice = r.choice(playable_deck)
+                player_choice = r.choice(playable_deck)
+
+                playable_deck.remove(dealer_choice)
+                playable_deck.remove(player_choice)
+
+                dealer_card = dealer_choice.split("-")
+                player_card = player_choice.split("-")
+
+                dealer = int(dealer_card[1])
+                player = int(player_card[1])
+
+                print(f"Dealer's Card: {dealer_card[0]}, Value: {dealer_card[1]}")
+                print(f"Player's Card: {player_card[0]}, Value: {player_card[1]}")
+                double_down = str(input("Would you like to double down? [Y, N] "))
+
+                if double_down == "Y":
+                    bet *= 2
+                    if bet > money:
+                        print("You don't have enough money to double down")
+                        playable_deck.append(dealer_choice)
+                        playable_deck.append(player_choice)
+                        break
+
+                    player_choice2 = r.choice(playable_deck)
+                    playable_deck.remove(player_choice2)
+                    player_card2 = player_choice2.split("-")
+                    player2 = int(player_card2[1])
+                    player += player2
+                    print(f"Player card: {player_card2}")
+                    print(f"Player total: {player}")
+                    while dealer < 17:
+                        print("Dealer Hit")
+                        dealer_choice2 = r.choice(playable_deck)
+                        playable_deck.remove(dealer_choice2)
+                        dealer_card2 = dealer_choice2.split("-")
+                        dealer2 = int(dealer_card2[1])
+                        dealer += dealer2
+                        print(f"Dealer's Card: {dealer_card2}")
+                        print(f"Dealer total: {dealer}")
+
+                        if dealer > 21:
+                            print("Dealer busted")
+                            money += bet
+                            break
+                        if dealer == 21:
+                            print("Dealer: Blackjack!")
+                            if player < 21:
+                                money -= bet
+
+                        if dealer == 21 and player == 21:
+                            print("Push")
+                            break
+                    if dealer != 21 and player == 21:
+                        money += bet * 1.5
+
+                    if player < 21 and dealer > player and dealer < 21:
+                        money -= bet
+
+                    if player < 21 and player > dealer and dealer < 21:
+                        money += bet
+                    break
+                while player < 21:
+
+                    hit_or_stand = str(input("Would you like to hit? [Y, N]:  "))
+                    if hit_or_stand == "Y":
+                        player_choice2 = r.choice(playable_deck)
+                        playable_deck.remove(player_choice2)
+                        player_card2 = player_choice2.split("-")
+                        player2 = int(player_card2[1])
+                        player += player2
+                        print(f"Player Card: {player_card2}")
+                        print(f"Player total: {player}")
+                    if player > 21:
+                        print("Player busted!")
+                        money -= bet
+                        break
+                    if player == 21:
+                        print("Player: Blackjack!")
+
+                    if hit_or_stand == "N":
+                        break
+
+                while dealer < 17:
+                    if player > 21:
+                        break
+                    dealer_choice2 = r.choice(playable_deck)
+                    playable_deck.remove(dealer_choice2)
+                    dealer_card2 = dealer_choice2.split("-")
+                    dealer2 = int(dealer_card2[1])
+                    dealer += dealer2
+                    print(f"Dealer's Card: {dealer_card2}")
+                    print(f"Dealer total: {dealer}")
+
+                    if dealer > 21:
+                        print("Dealer busted")
+                        money += bet
+                        break
+                    if dealer == 21:
+                        print("Dealer: Blackjack!")
+                        if player < 21:
+                            money -= bet
+
+                    if dealer == 21 and player == 21:
+                        print("Push")
+                        break
+                if dealer != 21 and player == 21:
+                    money += bet * 1.5
+
+                if player < 21 and dealer > player and dealer < 21:
+                    money -= bet
+
+                if player < 21 and player > dealer and dealer < 21:
+                    money += bet
+    except Exception as e:
+        print(f"{e}\nOut of cards, Game Over\n"
+              f"Your balance is {money}")
+
+
+blackjack()
+
+
+
+
+
+
+
+
